ee3d75e626f0c775f57d81c4f8d053e8c210a48b
Alpatkin Alexei <134586580+Bender1695@users.noreply.github.com>
2023-06-11 14:20:13 +0300

    Add files via upload
    добавил комментарии

--- homeworks/lvl_1/task_1.1.py
+++ homeworks/lvl_1/task_1.1.py
@@ -8,12 +8,10 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
 start_song=0
 a = []
-for i in range(len(my_favorite_songs)):
-    if my_favorite_songs[i] == ',':
-#        print(my_favorite_songs[start_song:i])
-        a.append (my_favorite_songs[start_song:i])
-        start_song = i+2
-a.append (my_favorite_songs[start_song:])
+for i in range(len(my_favorite_songs)): # цикл посимвольного перебора всего текста
+    if my_favorite_songs[i] == ',': # проверка на запятую, чтобы разделить трэки
+        a.append (my_favorite_songs[start_song:i]) # добавить в массив текст от его начала до найденной запятой
+        start_song = i+2 # установка начала нового текста
 print (a[0])
 print (a[-1])
 print (a[1])
